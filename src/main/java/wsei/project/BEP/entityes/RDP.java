5c3affe15d70e5f4bfd10de7578f0679fd209121
Oblitus <piotr5jurczak@gmail.com>
2022-04-25 00:48:08 +0200

    RDP Entity update.

--- src/main/java/wsei/project/BEP/entityes/RDP.java
+++ src/main/java/wsei/project/BEP/entityes/RDP.java
@@ -1,13 +1,16 @@
 package wsei.project.BEP.entityes;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.Objects;
+import java.util.UUID;
 
 @Document("rdps")
 public class RDP {
-    @MongoId
+    @Id
     private String id;
     @Field
     public String name;
@@ -19,4 +22,79 @@ public class RDP {
     @DocumentReference(collection = "types")
     public Type type;
 
+    public RDP(String name, String ipaddress, String password, Type type) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.ipaddress = ipaddress;
+        this.password = password;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIpaddress() {
+        return ipaddress;
+    }
+
+    public void setIpaddress(String ipaddress) {
+        this.ipaddress = ipaddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RDP)) return false;
+        RDP rdp = (RDP) o;
+        return getId().equals(rdp.getId()) &&
+                getName().equals(rdp.getName()) &&
+                getIpaddress().equals(rdp.getIpaddress()) &&
+                getPassword().equals(rdp.getPassword()) &&
+                getType().equals(rdp.getType());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getIpaddress(), getPassword(), getType());
+    }
+
+    @Override
+    public String toString() {
+        return "RDP{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", ipaddress='" + ipaddress + '\'' +
+                ", password='" + password + '\'' +
+                ", type=" + type +
+                '}';
+    }
+
+    public void generateRdpfile(){
+        //todo:thismethod
+    }
 }
