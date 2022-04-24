0edae41aad8bd4e87ac1ec3064492450423e54b0
Oblitus <piotr5jurczak@gmail.com>
2022-04-25 00:40:17 +0200

    Update of TeamViewer entity

--- src/main/java/wsei/project/BEP/entityes/TeamViewer.java
+++ src/main/java/wsei/project/BEP/entityes/TeamViewer.java
@@ -1,13 +1,16 @@
 package wsei.project.BEP.entityes;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.Objects;
+import java.util.UUID;
 
 @Document("teamviewers")
 public class TeamViewer {
-    @MongoId
+    @Id
     private String id;
     @Field
     public String name;
@@ -18,4 +21,76 @@ public class TeamViewer {
     @Field
     @DocumentReference(collection = "types")
     public Type type;
+
+    public TeamViewer(String name, String connectionId, String password, Type type) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.connectionId = connectionId;
+        this.password = password;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getConnectionId() {
+        return connectionId;
+    }
+
+    public void setConnectionId(String connectionId) {
+        this.connectionId = connectionId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TeamViewer)) return false;
+        TeamViewer that = (TeamViewer) o;
+        return getId().equals(that.getId()) &&
+                getName().equals(that.getName()) &&
+                getConnectionId().equals(that.getConnectionId()) &&
+                getPassword().equals(that.getPassword()) &&
+                getType().equals(that.getType());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getConnectionId(), getPassword(), getType());
+    }
+
+    @Override
+    public String toString() {
+        return "TeamViewer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", connectionId='" + connectionId + '\'' +
+                ", password='" + password + '\'' +
+                ", type=" + type +
+                '}';
+    }
 }
