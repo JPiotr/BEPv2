b8e02896876fbfbe0dbc0b1cae58e50f0bb77790
Oblitus <piotr5jurczak@gmail.com>
2022-04-29 18:03:59 +0200

    Client modules update.

--- src/main/java/wsei/project/BEP/mongo/ClientRepository.java
+++ src/main/java/wsei/project/BEP/mongo/ClientRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import wsei.project.BEP.entityes.Client;
+import wsei.project.BEP.entityes.DefaultStatus;
 
 import java.util.List;
 
@@ -11,5 +12,7 @@ public interface ClientRepository extends MongoRepository<Client,String> {
     <S extends Client> S findByCode(String code);
     <S extends Client> S findByName(String name);
     <S extends Client> S findByNumber(Integer number);
+
+    <E extends Client> List<E> findClientsByStatus(DefaultStatus status);
 
 }
