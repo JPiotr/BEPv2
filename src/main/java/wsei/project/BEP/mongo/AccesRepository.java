f76e731c10cc43ece618ac17414d3e67641c4b95
Oblitus <piotr5jurczak@gmail.com>
2022-04-29 18:51:44 +0200

    Cleaning.

--- src/main/java/wsei/project/BEP/mongo/AccesRepository.java
+++ src/main/java/wsei/project/BEP/mongo/AccesRepository.java
@@ -13,54 +13,5 @@ import java.util.function.Function;
 
 public interface AccesRepository extends MongoRepository<Acces,String> {
 
-    @Override
-    <S extends Acces> S save(S entity);
 
-    @Override
-    Optional<Acces> findById(String s);
-
-    @Override
-    boolean existsById(String s);
-
-    @Override
-    Iterable<Acces> findAllById(Iterable<String> strings);
-
-    @Override
-    long count();
-
-    @Override
-    void deleteById(String s);
-
-    @Override
-    void delete(Acces entity);
-
-    @Override
-    void deleteAllById(Iterable<? extends String> strings);
-
-    @Override
-    void deleteAll(Iterable<? extends Acces> entities);
-
-    @Override
-    void deleteAll();
-
-    @Override
-    <S extends Acces> List<S> saveAll(Iterable<S> entities);
-
-    @Override
-    List<Acces> findAll();
-
-    @Override
-    List<Acces> findAll(Sort sort);
-
-    @Override
-    <S extends Acces> S insert(S entity);
-
-    @Override
-    <S extends Acces> List<S> insert(Iterable<S> entities);
-
-    @Override
-    <S extends Acces> List<S> findAll(Example<S> example);
-
-    @Override
-    <S extends Acces> List<S> findAll(Example<S> example, Sort sort);
 }
