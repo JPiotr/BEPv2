5ad665fdc4dcfde604b1e36ea4508f6c3114bfa5
Oblitus <piotr5jurczak@gmail.com>
2022-04-30 19:23:56 +0200

    Vpn Repo update

--- src/main/java/wsei/project/BEP/mongo/Access/VPNRepository.java
+++ src/main/java/wsei/project/BEP/mongo/Access/VPNRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import wsei.project.BEP.entityes.Access.VPN;
 
 public interface VPNRepository extends MongoRepository<VPN, String> {
+<S extends  VPN> S findByName(String name);
+<S extends  VPN> S findByIpaddress(String ip);
 
 }
