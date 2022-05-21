3d6d3c7bcf18530abd81f8c0f3f39ac07c17d029
Oblitus <piotr5jurczak@gmail.com>
2022-05-21 19:39:32 +0200

    Building Access Controller

--- src/main/java/wsei/project/BEP/controllers/Access/AccesController.java
+++ src/main/java/wsei/project/BEP/controllers/Access/AccesController.java
@@ -1,8 +1,16 @@
 package wsei.project.BEP.controllers.Access;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wsei.project.BEP.services.Access.AccessService;
 
-@Controller
+@RestController
+@RequestMapping("/access")
+@AllArgsConstructor
 public class AccesController {
+    private final AccessService serv;
+
 
 }
